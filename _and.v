290be22173f7bd9cf4db46fc47653ce1187aa109
Ernesto-Javier-Tejeda-Hermoso <javiertejeda376@gmail.com>
2025-08-18 15:43:50 -0600

    Se agrega el Proyecto

---
+++ _and.v
@@ -0,0 +1,10 @@
+//1. Definicion del modulo y sus entradas y salidas
+module _and (input A, input B, output C);
+//2. declarar señales/elementos internos
+//NO aplica en este ejemplo
+//3. Comportamiento del modulo 
+//(asignaciones, instancias, conexiones)
+assign C = A&B;
+
+
+endmodule
